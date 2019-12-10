082dd1fc0a63b4129bbd3740b66c4b2313d9fa67
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-10 15:31:01 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(name)
+  puts "Hi, my name is #{name}\n."
+end
+
+def introduction_with_language("Dan", "Ember.js")
+  puts "Hi, my name is #{introduction_with_language} and I am learning to program in #{introduction_with_language|\n"
+end
