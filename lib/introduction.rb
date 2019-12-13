e66b4dd133a5656538e83b5b3a5008f63bda8d06
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-13 00:32:40 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,5 +6,5 @@ def introduction(name)
 end
 
 def introduction_with_language("Dan", "Ember.js")
-  puts "Hi, my name is #{introduction_with_language} and I am learning to program in #{introduction_with_language|\n"
+  puts "Hi, my name is #{introduction_with_language} and I am learning to program in #{introduction_with_language}\n"
 end
